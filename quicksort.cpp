015be5f3700e1d069101228187b70f92260a1ec7
krish mamtora <krishmamtora26@gmail.com>
2025-06-18 00:33:18 +0530

    quick sort

---
+++ quicksort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+
+    while (i <= j)
+    {
+        while (i <= high && arr[i] <= pivot)
+        {
+            i++;
+        }
+        while (j >= low && arr[j] > pivot)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quickSort(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        cout << "pivot : " << pi << endl;
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(const vector<int> &arr)
+{
+    for (int num : arr)
+        cout << num << " ";
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {7, 3, 2, 1, 9, 10, 11, 5};
+    int n = arr.size();
+
+    cout << "Original array: ";
+    printArray(arr);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
